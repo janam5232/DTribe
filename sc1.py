543d62f4405e432063a0bbe2c00dd31602e798d6
janam5232 <32261756+janam5232@users.noreply.github.com>
2021-07-10 18:24:15 +0530

    iteration 1 (extration)

--- sc1.py
+++ sc1.py
@@ -1,10 +1,14 @@
+#imported libraries
 import pandas as pd
 import os
 import glob as gb
 
+#getting the current directory's location
 path = os.getcwd()
+#getting the name of all the excel files
 files = gb.glob(os.path.join(path, "*.xlsx"))
 
+#looping through the directory and extracting all the excel files' data
 for f in files:
     df = pd.read_excel(f, sheet_name="Regression Model")
     print("Location: ", f)

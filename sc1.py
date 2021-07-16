1efd73034c84f83b2241e955b5cf5b20dd923018
janam5232 <32261756+janam5232@users.noreply.github.com>
2021-07-17 02:51:24 +0530

    Update sc1.py

--- sc1.py
+++ sc1.py
@@ -1,18 +1,37 @@
-#imported libraries
+import numpy as np
 import pandas as pd
-import os
 import glob as gb
+import os
 
 #getting the current directory's location
 path = os.getcwd()
+
 #getting the name of all the excel files
 files = gb.glob(os.path.join(path, "*.xlsx"))
 
+# name = r"^R{1}[a-zA-Z\s]+$"
+dictionaryOfdataframesOfExcelFiles = {}
+fileNamesList = []
+oneEMRMDF = []
+manyEMRMDF = []
+databaseDF = []
+i = 0
+
 #looping through the directory and extracting all the excel files' data
 for f in files:
-    df = pd.read_excel(f, sheet_name="Regression Model")
-    print("Location: ", f)
-    print('File Name: ', f.split("\\")[-1])
+    file_name = f.split('\\')[-1]
+    fileNamesList.append(file_name.split()[0])
+    print(f)
+    if(len(fileNamesList[i]) == 4):
+        oneEMRMDF.append(pd.read_excel(f, sheet_name = (["Regression Model", "Empirical Model", "Data"])))
+        print(fileNamesList[i])
+    if(len(fileNamesList[i]) == 3 or len(fileNamesList[i]) == 2):
+        manyEMRMDF.append(pd.read_excel(f, sheet_name = None))
+        print(fileNamesList[i])
+    i += 1
 
-    print('Content: ')
-    print(df.head())
+# for f in files:
+#     databaseDF.append(pd.read_excel(f, sheet_name = ('Data')))
+#     print(databaseDF[i])
+#     i = i + 1
+# while(i<len(files)):

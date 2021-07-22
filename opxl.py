4cd754cadbe61c48a93a71d71a56a2e55f7e04cf
janam5232 <32261756+janam5232@users.noreply.github.com>
2021-07-23 05:07:31 +0530

    Update opxl.py

--- opxl.py
+++ opxl.py
@@ -50,72 +50,72 @@ for file in files:
     regressionModelSheets = [sheets for sheets in allWorksheetsInTheFile if "Regression Model" in sheets]
 
     #sheets with emperical model data
-    # for sheet in empericalModelSheets:
-    #     workableSheet = workbook[sheet]
-    #     print(sheet)
-    #     for row in workableSheet['D1':'D' + str(workableSheet.max_row)]:
-    #         for cellValue in row:
-    #             tempStr = str(cellValue.value)
-    #             if "Estimated total sold" in tempStr and tempStr[-3] == "Q":
-    #                 estimatedTotalSold = workableSheet[get_column_letter(cellValue.column + 2) + str(cellValue.row)]
-    #                 estimatedMaxSold = workableSheet[get_column_letter(cellValue.column + 2) + str(cellValue.row + 1)]
-    #                 estimatedMinSold = workableSheet[get_column_letter(cellValue.column + 2) + str(cellValue.row + 2)]
+    for sheet in empericalModelSheets:
+        workableSheet = workbook[sheet]
+        print(sheet)
+        for row in workableSheet['D1':'D' + str(workableSheet.max_row)]:
+            for cellValue in row:
+                tempStr = str(cellValue.value)
+                if "Estimated total sold" in tempStr and tempStr[-3] == "Q":
+                    estimatedTotalSold = workableSheet[get_column_letter(cellValue.column + 2) + str(cellValue.row)]
+                    estimatedMaxSold = workableSheet[get_column_letter(cellValue.column + 2) + str(cellValue.row + 1)]
+                    estimatedMinSold = workableSheet[get_column_letter(cellValue.column + 2) + str(cellValue.row + 2)]
 
-    #                 print(estimatedTotalSold.value)
-    #                 print(estimatedMaxSold.value)
-    #                 print(estimatedMinSold.value)
+                    print(estimatedTotalSold.value)
+                    print(estimatedMaxSold.value)
+                    print(estimatedMinSold.value)
 
-    #                 outputWorkbookWorksheet["F"+str(rowCounterEmpirical)] = estimatedTotalSold.value
-    #                 print("F" + str(rowCounterEmpirical))
-    #                 outputWorkbookWorksheet["G"+str(rowCounterEmpirical)] = estimatedMaxSold.value
-    #                 print("G" + str(rowCounterEmpirical))
-    #                 outputWorkbookWorksheet["H"+str(rowCounterEmpirical)] = estimatedMinSold.value
-    #                 print("H" + str(rowCounterEmpirical))
-    #                 if sheet[-5:] != "Model" :
-    #                     sheetNameTmp = sheet.split('-')
-    #                     print(sheetNameTmp[1].strip())
-    #                     outputWorkbookWorksheet["C"+str(rowCounterEmpirical)] = sheetNameTmp[1].strip()
-    #                 else:
-    #                     outputWorkbookWorksheet["C"+str(rowCounterEmpirical)] = "Null"
-    #                 rowCounterEmpirical = rowCounterEmpirical + 1
+                    outputWorkbookWorksheet["F"+str(rowCounterEmpirical)] = estimatedTotalSold.value
+                    print("F" + str(rowCounterEmpirical))
+                    outputWorkbookWorksheet["G"+str(rowCounterEmpirical)] = estimatedMaxSold.value
+                    print("G" + str(rowCounterEmpirical))
+                    outputWorkbookWorksheet["H"+str(rowCounterEmpirical)] = estimatedMinSold.value
+                    print("H" + str(rowCounterEmpirical))
+                    if sheet[-5:] != "Model" :
+                        sheetNameTmp = sheet.split('-')
+                        print(sheetNameTmp[1].strip())
+                        outputWorkbookWorksheet["C"+str(rowCounterEmpirical)] = sheetNameTmp[1].strip()
+                    else:
+                        outputWorkbookWorksheet["C"+str(rowCounterEmpirical)] = "Null"
+                    rowCounterEmpirical = rowCounterEmpirical + 1
     
     #sheets with regression model data
-#     for sheet in regressionModelSheets:
-#         workableSheet = workbook[sheet]
-#         print(sheet)
-#         for row in workableSheet["C1":"R" + str(workableSheet.max_row)]:
-#             for colObj in row:
-#                 tmpValue = str(colObj.value)
-#                 if tmpValue.strip() == "Max":
-#                     quarter = workableSheet["D" + str(colObj.row - 1)].value
-#                     yeartmp = workableSheet["C" + str(colObj.row -1)].value
-#                     year = "20" + yeartmp[-2:]
-#                     forecastSA = workableSheet[get_column_letter(colObj.column + 1) + str(colObj.row - 1)].value
-#                     forecastMin = workableSheet[get_column_letter(colObj.column + 1) + str(colObj.row + 1)].value
-#                     forecastMax = workableSheet[get_column_letter(colObj.column + 1) + str(colObj.row)].value
-#                     outputWorkbookWorksheet["A"+str(rowCounterRegression)] = curDate.strftime("%Y-%m-%d")
-#                     print("A" + str(rowCounterRegression))
-#                     outputWorkbookWorksheet["D"+str(rowCounterRegression)] = quarter
-#                     print("D" + str(rowCounterRegression))
-#                     outputWorkbookWorksheet["B"+str(rowCounterRegression)] = ticker
-#                     print("B" + str(rowCounterRegression))
-#                     outputWorkbookWorksheet["E"+str(rowCounterRegression)] = year
-#                     print("E" + str(rowCounterRegression))
-#                     outputWorkbookWorksheet["I"+str(rowCounterRegression)] = forecastSA
-#                     print("I" + str(rowCounterRegression))
-#                     outputWorkbookWorksheet["J"+str(rowCounterRegression)] = forecastMax
-#                     print("J" + str(rowCounterRegression))
-#                     outputWorkbookWorksheet["K"+str(rowCounterRegression)] = forecastMin
-#                     print("K" + str(rowCounterRegression))
-#                     if sheet[-5:] != "Model" :
-#                         sheetNameTmp = sheet.split('-')
-#                         print(sheetNameTmp[1].strip())
-#                         outputWorkbookWorksheet["C"+str(rowCounterRegression)] = sheetNameTmp[1].strip()
-#                     else:
-#                         outputWorkbookWorksheet["C"+str(rowCounterRegression)] = "Null"
+    for sheet in regressionModelSheets:
+        workableSheet = workbook[sheet]
+        print(sheet)
+        for row in workableSheet["C1":"R" + str(workableSheet.max_row)]:
+            for colObj in row:
+                tmpValue = str(colObj.value)
+                if tmpValue.strip() == "Max":
+                    quarter = workableSheet["D" + str(colObj.row - 1)].value
+                    yeartmp = workableSheet["C" + str(colObj.row -1)].value
+                    year = "20" + yeartmp[-2:]
+                    forecastSA = workableSheet[get_column_letter(colObj.column + 1) + str(colObj.row - 1)].value
+                    forecastMin = workableSheet[get_column_letter(colObj.column + 1) + str(colObj.row + 1)].value
+                    forecastMax = workableSheet[get_column_letter(colObj.column + 1) + str(colObj.row)].value
+                    outputWorkbookWorksheet["A"+str(rowCounterRegression)] = curDate.strftime("%Y-%m-%d")
+                    print("A" + str(rowCounterRegression))
+                    outputWorkbookWorksheet["D"+str(rowCounterRegression)] = quarter
+                    print("D" + str(rowCounterRegression))
+                    outputWorkbookWorksheet["B"+str(rowCounterRegression)] = ticker
+                    print("B" + str(rowCounterRegression))
+                    outputWorkbookWorksheet["E"+str(rowCounterRegression)] = year
+                    print("E" + str(rowCounterRegression))
+                    outputWorkbookWorksheet["I"+str(rowCounterRegression)] = forecastSA
+                    print("I" + str(rowCounterRegression))
+                    outputWorkbookWorksheet["J"+str(rowCounterRegression)] = forecastMax
+                    print("J" + str(rowCounterRegression))
+                    outputWorkbookWorksheet["K"+str(rowCounterRegression)] = forecastMin
+                    print("K" + str(rowCounterRegression))
+                    if sheet[-5:] != "Model" :
+                        sheetNameTmp = sheet.split('-')
+                        print(sheetNameTmp[1].strip())
+                        outputWorkbookWorksheet["C"+str(rowCounterRegression)] = sheetNameTmp[1].strip()
+                    else:
+                        outputWorkbookWorksheet["C"+str(rowCounterRegression)] = "Null"
                     
-#                     rowCounterRegression = rowCounterRegression + 1
+                    rowCounterRegression = rowCounterRegression + 1
 
-# outputWorkbook.save(outputWorkbookPath)
+outputWorkbook.save(outputWorkbookPath)
 
 #database storage part

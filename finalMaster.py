03e42e681afc563b693e6ae28755b456a63967f5
janam5232 <32261756+janam5232@users.noreply.github.com>
2021-08-14 17:04:21 +0530

    Update finalMaster.py

--- finalMaster.py
+++ finalMaster.py
@@ -8,9 +8,9 @@ from datetime import datetime as dt
 import io
 import timeit
 import mysql.connector as sql
+import pandas as pd
 
 #DATABASE CONNECTION
-
 dbName = 'AnalystData'
 host = 'db1.cs3pcss752yf.ap-south-1.rds.amazonaws.com'
 user = 'admin'
@@ -24,43 +24,51 @@ mydb = sql.connect(
 )
 
 cur = mydb.cursor()
-print(mydb)
+
+mydbData = sql.connect(
+        host = "db1.cs3pcss752yf.ap-south-1.rds.amazonaws.com",
+        user = "admin",
+        passwd = "janam12345",
+        database = "Data"
+    )
+
+curData = mydbData.cursor()
+curData.fast_executemany = True
+print(mydbData)
+#Opening LOG File
+logFileNameDate = str(dt.now())
+logFileNameDate = logFileNameDate.replace('.', '').replace(' ', '').replace(':', '-')
+logFile = open("C:\\Users\\janam\\Desktop\\output\\" + "ETL-LogFile-" + logFileNameDate + ".txt", 'a')
+logFile.writelines('MySQL connection: \n\n')
+logFile.writelines(str(mydb) + '\n')
 
 #getting the current directories locaiton (where the data will be)
-zipPath = "C:\\Users\\janam\\Desktop\\files"
+zipPath = "C:\\Users\\janam\\Desktop\\files\\"
 
 #accessing the zip file
-file = gb.glob(os.path.join(zipPath + '\\', "*.zip"))
+file = gb.glob(os.path.join(zipPath, "*.zip"))
 
 #extracting the zip file
 zip = zp.ZipFile(file[0])
-zip.extractall("C:\\Users\\janam\\Desktop\\files")
+zip.extractall(zipPath)
 
 zip.close()
-
+logFile.writelines('Files Extracted from ' + file[0] + '\n\n')
 #checking if files already exist
 #need to change directory paths
-outputPath = "C:\\Users\\janam\\Desktop\\output"
+outputPath = "C:\\Users\\janam\\Desktop\\output\\"
 curDate = dt.today()
 fileName = "model_" + curDate.strftime("%Y%m%d") + ".xlsx"
-if os.path.isfile(outputPath + '\\' + fileName):
-    # created = os.stat(fileName).st_ctime
-    # now = dt.now()
-    # dateTimeFile = dt.fromtimestamp(created)
-
-    # dateFile = dateTimeFile.strftime("%m/%d/%Y")
-
-    # dateNow = now.strftime("%m/%d/%Y")
-
-    # if(dateNow == dateFile):
-    os.remove(outputPath + '\\' + fileName)
+if os.path.isfile(outputPath + fileName):
+    os.remove(outputPath + fileName)
     print("fileRemoved")
 
 curDate = dt.today()
 outputFileName = "model_" + curDate.strftime("%Y%m%d")
 print(outputFileName)
-outputWorkbookPath = outputPath+'\\'+outputFileName+".xlsx"
+outputWorkbookPath = outputPath + outputFileName+".xlsx"
 outputWorkbook = Workbook()
+logFile.writelines('Output workbook created.\n\n')
 outputWorkbookWorksheet = outputWorkbook.active
 outputWorkbookWorksheet['A1'] = 'Date'
 outputWorkbookWorksheet['B1'] = 'Ticker'
@@ -84,9 +92,10 @@ rowCounterEmpirical = 2
 rowCounterRegression = 2
 
 tic = timeit.default_timer()
+logFile.writelines('Extraction starts here: \n\n')
 for file in files:
     ticker = file.split(' ')[0]
-    with open(zipPath + '\\' + file, "rb") as f:
+    with open(zipPath + file, "rb") as f:
         in_file = io.BytesIO(f.read())
     workbook = op.load_workbook(in_file, read_only=True, data_only=True)
     allWorksheetsInTheFile = workbook.sheetnames
@@ -96,6 +105,8 @@ for file in files:
     regressionModelSheets = [sheets for sheets in allWorksheetsInTheFile if "Regression Model" in sheets]
 
     #sheets with emperical model data
+    logFile.writelines('\t\t\t\tFileName: ' + file + '\n\n')
+    innertic = timeit.default_timer()
     for sheet in empericalModelSheets:
         workableSheet = workbook[sheet]
         print(sheet)
@@ -119,13 +130,17 @@ for file in files:
                     # print("H" + str(rowCounterEmpirical))
                     if sheet[-5:] != "Model" :
                         sheetNameTmp = sheet.split('-')
-                        print(sheetNameTmp[1].strip())
                         outputWorkbookWorksheet["C"+str(rowCounterEmpirical)] = sheetNameTmp[1].strip()
                     else:
                         outputWorkbookWorksheet["C"+str(rowCounterEmpirical)] = "Null"
                     rowCounterEmpirical = rowCounterEmpirical + 1
-    
+    innertoc = timeit.default_timer()
+    pTime = str(innertoc - innertic)
+
+    logFile.writelines('Time taken by empirical sheets in ' + file + ' : ' + pTime + '\n\n')
+
     #sheets with regression model data
+    innertic = timeit.default_timer()
     for sheet in regressionModelSheets:
         workableSheet = workbook[sheet]
         print(sheet)
@@ -161,15 +176,61 @@ for file in files:
                         outputWorkbookWorksheet["C"+str(rowCounterRegression)] = "Null"
                     
                     rowCounterRegression = rowCounterRegression + 1
+    innertoc = timeit.default_timer()
+    pTime = str(innertoc - innertic)
+    logFile.writelines('Time taken by Regression sheets in ' + file + ' : ' + pTime + '\n\n') 
 
+    innerDataTic = timeit.default_timer()
+    logFile.writelines('Uploading "Data" sheet from ' + file + ' to database \n\n')
+    for sheet in data:
+        finalDFColumnList = []
+        ogColList = ['Date', 'FacilityType', 'BedSize', 'Region', 'Manufacturer', 'Ticker', 'Group', 'Therapy', 'Anatomy','SubAnatomy', 'ProductCategory', 'Quantity', 'AvgPrice', 'TotalSpend']
+        path = "C:\\Users\\janam\\Desktop\\ETL Datasheet\\"
+        eFiles = os.listdir(path)
+        eFiles = [file for file in eFiles if ".xlsx" in file]
+
+        df = pd.read_excel(zipPath + file, sheet_name=sheet)
+        dfColumnList = df.columns.tolist()
+        for column in dfColumnList:
+            if column in ogColList:
+                finalDFColumnList.append(column)
+        maxDate = df.tail(1)
+        maxD = maxDate.iloc[0]['Date']
+        finalDate = df.loc[(df['Date'] == maxD)]
+        dateFilter = df['Date'] == maxD
+        try:
+            myData = []
+            query = ''
+            finalUpdatedColList = "`,`".join([i for i in finalDFColumnList])
+
+            FinalDataFrame = df.loc[(df['Date'] == maxD),finalDFColumnList]
+            
+            FinalDataFrame = FinalDataFrame.astype(str)
+            
+            qTuple = list(FinalDataFrame.itertuples(index=False, name=None))
+        
+            for i,row in FinalDataFrame.iterrows():
+                query = "INSERT INTO `lastMonthData` (`" +finalUpdatedColList+ "`) VALUES("+ "%s,"*(len(row)-1)+"%s)"
+                break
+            curData.executemany(query, qTuple)
+            mydbData.commit()
+        except Exception as e:
+            error = str(e)
+            logFile.writelines('Error: ' + error + '\n\n')
+            
+    innerDataToc = timeit.default_timer()
+    dataFinalTime = str(innerDataToc - innerDataTic)
+    logFile.writelines('Time taken to upload "Data" sheet to database: ' + dataFinalTime + '\n\n')
 outputWorkbook.save(outputWorkbookPath)
 
 #deleting files after processing
-
 for file in files:
-    os.remove(zipPath + '\\' + file)
+    os.remove(zipPath + file)
 
-with open(outputPath + '\\' + fileName, "rb") as f:
+#uploading data to the database
+logFile.writelines('Uploading data to the database\n\n')
+uploadTimer1 = timeit.default_timer()
+with open(outputPath + fileName, "rb") as f:
         in_file = io.BytesIO(f.read())
 finalDataFile = op.load_workbook(in_file, read_only=True)
 finalSheet = finalDataFile.active
@@ -185,8 +246,15 @@ q = "INSERT INTO Data(Date, Ticker, Type, Quarter, Year, EstimatedTotalSold, Est
 cur.executemany(q, rowValues)
 mydb.commit()
 mydb.close()
-print(rowValues)
+mydbData.close()
+uploadTimer2 = timeit.default_timer()
+uploadTime = str(uploadTimer2 - uploadTimer1)
+logFile.writelines('Time taken to upload the final data to database : ' + uploadTime + '\n\n')
+# archiveDestinationPath = "/opt/eVolume/datout/archive/"
+# su.move(file[0], archiveDestinationPath)
 
 toc = timeit.default_timer()
 
-print(toc-tic)
+finalPTime = str(toc-tic)
+logFile.writelines('Time taken by the whole script : ' + finalPTime)
+logFile.close()
